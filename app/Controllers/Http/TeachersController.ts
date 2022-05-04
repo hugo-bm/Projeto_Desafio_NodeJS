cce248e59c17634b1a011c56c16296bece171fc6
Hugo Batalha Moreno <hugomb123@gmail.com>
2022-05-04 16:54:31 -0300

    :bug: fix: Aplicado correções no Model, Migration de Teacher

--- app/Controllers/Http/TeachersController.ts
+++ app/Controllers/Http/TeachersController.ts
@@ -1,13 +1,28 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Teacher from 'App/Models/Teacher'
 
 export default class TeachersController {
   public async show() {}
   public async store({ request, response }: HttpContextContract) {
     const body: Record<string, any> = request.body()
-    let { nome } = body
-    response.status(201)
+    let { nome, email, matricula, nascimento } = body
+
+    try {
+      let teacher: Teacher = new Teacher()
+      teacher.nome = nome
+      teacher.email = email
+      teacher.matricula = matricula
+      teacher.nascimento = nascimento
+      await teacher.save()
+      response.status(201)
+    } catch (err: any) {
+      response.status(500)
+      return {
+        message: 'Error ao concluir o processo de cadastro, contate seu administrador!',
+      }
+    }
     return {
-      teste: `endpoint_${nome}`,
+      message: `${nome} cadastrado com sucesso!`,
     }
   }
   public async update() {}
--- app/Models/Teacher.ts
+++ app/Models/Teacher.ts
@@ -1,3 +1,5 @@
 import Person from './Person'
 
-export default class Teacher extends Person {}
+export default class Teacher extends Person {
+  public static table = 'teachers'
+}
